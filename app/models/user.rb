051fe391ffbd9b5b39ba38172c69ec0158cf3e16
David Lormor <dlormor@aidt.edu>
2014-02-06 13:46:27 -0600

    Updated email and username validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,14 @@ class User < ActiveRecord::Base
 
 	validates :username, presence: true, length: { maximum: 25 },
 						uniqueness: { case_sensitive: false }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 	validates :password, length: { minimum: 8 }
 	has_secure_password
 
 	protected
 		def downcase_username_and_email
-			self.email = email.downcase
-			self.username = username.downcase
+			email.downcase!
+			username.downcase!
 		end
 end
