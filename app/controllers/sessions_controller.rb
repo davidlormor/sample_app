abde0486c981d53011ac842eb37820524b0ed08a
David Lormor <dlormor@aidt.edu>
2014-02-07 12:15:38 -0600

    Eliminated form_for on sign in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by(username: params[:session][:username].downcase)
-		if user && user.authenticate(params[:session][:password])
+		user = User.find_by(username: params[:username].downcase)
+		if user && user.authenticate(params[:password])
 			sign_in user
 			redirect_to user
 		else
